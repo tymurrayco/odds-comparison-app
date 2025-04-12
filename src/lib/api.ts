787a3213a3171b20494840b7a1dd71ae98e79a7f
Tyler <tymurrayco@gmail.com>
2025-04-12 07:54:52 -0700

    Adding logos

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -66,7 +66,7 @@ export const LEAGUES = [
   { id: 'americanfootball_ncaaf', name: 'NCAAF', icon: '/league-icons/ncaaf.png' },
   { id: 'soccer_epl', name: 'EPL', icon: '/league-icons/epl.png' },
   { id: 'golf_masters_tournament_winner', name: 'Masters', icon: '/league-icons/masters.png' },
-  { id: 'lacrosse_ncaa', name: 'NCAAL', icon: '/league-icons/ncaal.png' },
+  { id: 'lacrosse_ncaa', name: 'NCAAL', icon: '/league-icons/ncaal.png' }, 
 ];
 
 /**
