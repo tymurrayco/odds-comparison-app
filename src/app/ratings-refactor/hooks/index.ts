ab2bf895da3930e5476b1e0d9eba80c9f71798b5
Tyler <tymurrayco@gmail.com>
2026-02-01 19:06:03 -0700

    Pushing up refactored everything

--- src/app/ratings-refactor/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-// src/app/ratings/hooks/index.ts
-export { useDebounce } from './useDebounce';
-export { useRatingsData } from './useRatingsData';
-export type { UseRatingsDataReturn } from './useRatingsData';
